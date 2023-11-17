0891ca2c1511810c51175905deb5f2c8ece7859e
netdragonx <netdragon0x@gmail.com>
2023-11-17 00:13:36 -0800

    Updated deploy script

---
+++ deploy.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+. .env.local
+
+if [ -z "$ALCHEMY_API_KEY" ]; then
+    echo "Missing ALCHEMY_API_KEY"
+    exit 1
+fi
+
+anvil() {
+    forge script script/DeployAllTestnet.s.sol:DeployAllTestnet \
+        -vvvv \
+        --fork-url http://localhost:8545 \
+        --optimize \
+        --optimizer-runs 10000 \
+        --gas-estimate-multiplier 200 \
+        --sender $DEPLOYER_ADDRESS \
+        --interactives 1 \
+        --broadcast
+}
+
+testnet() {
+    local rpcUrl=$1
+    
+    if [ -z "$rpcUrl" ]; then
+        echo "Missing rpcUrl"
+        exit 1
+    fi
+
+    if [ -z "$DEPLOYER_ADDRESS" ]; then
+        echo "Missing DEPLOYER_ADDRESS"
+        exit 1
+    fi
+
+    forge script script/DeployAllTestnet.s.sol:DeployAllTestnet \
+        -vvvv \
+        --rpc-url "$rpcUrl" \
+        --optimize \
+        --optimizer-runs 10000 \
+        --gas-estimate-multiplier 200 \
+        --verify \
+        --sender "$DEPLOYER_ADDRESS" \
+        --interactives 1 \
+        --broadcast
+}
+
+mainnet() {
+    local rpcUrl=$1
+
+    if [ -z "$rpcUrl" ]; then
+        echo "Missing rpcUrl"
+        exit 1
+    fi
+
+    if [ -z "$DEPLOYER_ADDRESS" ]; then
+        echo "Missing DEPLOYER_ADDRESS"
+        exit 1
+    fi
+
+    forge script script/DeployAllMainnet.s.sol:DeployAllMainnet \
+        -vvvv \
+        --rpc-url "$rpcUrl" \
+        --optimize \
+        --optimizer-runs 10000 \
+        --gas-estimate-multiplier 200 \
+        --verify \
+        --sender "$DEPLOYER_ADDRESS" \
+        --interactives 1 \
+        --broadcast
+}
+
+case $1 in
+    anvil)
+        anvil
+        ;;
+    sepolia)
+        testnet https://eth-sepolia.g.alchemy.com/v2/$ALCHEMY_API_KEY
+        ;;
+    goerli)
+        testnet https://eth-goerli.g.alchemy.com/v2/$ALCHEMY_API_KEY
+        ;;
+    mumbai)
+        testnet https://polygon-mumbai.g.alchemy.com/v2/$ALCHEMY_API_KEY
+        ;;
+    mainnet)
+        mainnet https://eth-mainnet.g.alchemy.com/v2/$ALCHEMY_API_KEY
+        ;;
+    polygon)
+        mainnet https://polygon-mainnet.g.alchemy.com/v2/$ALCHEMY_API_KEY
+        ;;
+    optimism)
+        mainnet https://opt-mainnet.g.alchemy.com/v2/$ALCHEMY_API_KEY
+        ;;
+    arbitrum)
+        mainnet https://arb-mainnet.g.alchemy.com/v2/$ALCHEMY_API_KEY
+        ;;
+    *)
+        echo "Usage: $0 {anvil|sepolia|goerli|mumbai|mainnet|polygon|optimism|arbitrum}"
+        exit 1
+esac
+
+exit 0
